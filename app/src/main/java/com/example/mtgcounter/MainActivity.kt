5cba7480be6dd498dcd549c520c568d1c2f157a5
doug <dougparker.m@gmail.com>
2021-08-17 01:22:52 -0230

    Override onClick. Working on making each button work independently through the "when"

--- app/src/main/java/com/example/mtgcounter/MainActivity.kt
+++ app/src/main/java/com/example/mtgcounter/MainActivity.kt
@@ -5,10 +5,35 @@ package com.example.mtgcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.mtgcounter.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            binding.btnUpScore1.id ->{
+                scoreUp(v)
+            }
+            binding.btnUpScore2.id ->{
+                binding.tvDisplayScore2.text = "dog"
+            }
+        }
+    }
+
+
+    fun scoreUp(view: View) {
+//        var currentID = view.
+
+        var score = binding.tvDisplayScore1.text.toString()
+        binding.tvDisplayScore1.text = (score.toInt() + 1).toString()
     }
 }
